04032a1369ce9bc48e5b5515e6cf240349861fa6
Thiago <88464208+thiago-pari@users.noreply.github.com>
2021-08-17 19:19:52 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,4 @@
-#******************************************************************
 #Step 0. Downloading and unzipping dataset
-#******************************************************************
 
 if(!file.exists("./data")){dir.create("./data")}
 #Here are the data for the project:
@@ -12,9 +10,7 @@ unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
 #You should create one R script called run_analysis.R that does the following.
 
-#******************************************************************
 #Step 1.Merges the training and the test sets to create one data set.
-#******************************************************************
 
 # 1.1 Reading files
 
@@ -56,9 +52,7 @@ setAllInOne <- rbind(mrg_train, mrg_test)
 #dim(setAllInOne)
 #[1] 10299   563
 
-#******************************************************************
 #Step 2.-Extracts only the measurements on the mean and standard deviation for each measurement.
-#******************************************************************
 
 #2.1 Reading column names:
 
@@ -76,23 +70,17 @@ mean_and_std <- (grepl("activityId" , colNames) |
 
 setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
 
-#******************************************************************
 #Step 3. Uses descriptive activity names to name the activities in the data set
-#******************************************************************
 
 setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
                               by='activityId',
                               all.x=TRUE)
 
-#******************************************************************
 #Step 4. Appropriately labels the data set with descriptive variable names.
-#******************************************************************
 
 #Done in previous steps, see 1.3,2.2 and 2.3!
 
-#******************************************************************
 #Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-#******************************************************************
 
 #5.1 Making a second tidy data set
 
